b1bb141412b7cd0f1d18eeccaa528873321db69f
Jordan Hudgens <jordan@devcamp.com>
2017-06-14 16:14:09 +0000

    new actions for portfolios

--- MyApp/app/controllers/portfolios_controller.rb
+++ MyApp/app/controllers/portfolios_controller.rb
@@ -1,5 +1,23 @@
 class PortfoliosController < ApplicationController
   def index 
-    @portflio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
+  
+  def new 
+    @portfolio_item = Portfolio.new
+  end
+  
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :sutitle, :body))
+    
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
 end
