dd57facc93914c84641849caec98cc7b365f4c88
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 15:18:53 +0000

    Implemented initial file uploading system via carrierwave for portfolio items

--- MyApp/app/controllers/portfolios_controller.rb
+++ MyApp/app/controllers/portfolios_controller.rb
@@ -72,6 +72,8 @@ class PortfoliosController < ApplicationController
     params.require(:portfolio).permit(:title,
                                       :sutitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
                                     )
   end
